bdcb7eb5d2b137579e1878f1bf80d3a0dfa7ba2b
Lucas Martins <lucasifbaeuna@gmail.com>
2025-07-22 19:53:20 -0300

    feat[register-page]: Validation register-page

--- src/app/pages/register-page/register-page.ts
+++ src/app/pages/register-page/register-page.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-import { Header } from '../../shared/header/header';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-page.html',
   styleUrl: './register-page.css',
 })
-export class RegisterPage {}
+export class RegisterPage {
+  registerForm = new FormGroup({
+    name: new FormControl('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      nonNullable: true,
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+      nonNullable: true,
+    }),
+  });
+
+  submitForm() {
+    if (this.registerForm.valid) {
+      console.log('Form Submitted', this.registerForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
